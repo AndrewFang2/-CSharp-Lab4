4ee9f457788708b2a0802277b325fc0ecd6f1f6b
AndrewFang2 <AndrewFang6@gmail.com>
2021-03-13 03:52:35 -0500

    commit

---
+++ Lab4/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Lab4.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolCommunityContext _context;
+
+        public HomeController(SchoolCommunityContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
